0eaa057e0128ab8ccd1e1c181bb20a19cc5c0ee7
Tom ten Thij <git-naked@tomtenthij.nl>
2008-04-12 16:16:13 +0100

    Updated to WWW SQL Designer v2.0.1

--- generators/schema_browser/schema_browser_generator.rb
+++ generators/schema_browser/schema_browser_generator.rb
@@ -2,42 +2,51 @@ class SchemaBrowserGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       images = ["back.gif",
-                "h_1-n.gif",
-                "h_n-1.gif",
-                "move_cross.gif",
-                "shadow_bottom.png",
-                "shadow_corner.png",
-                "shadow.png",
-                "shadow_right.png",
-                "v_1-n.gif",
-                "v_n-1.gif"]
+                "bar_bottom.png",
+                "bar_corner.png",
+                "bar_left.png",
+                "table_bottom.png",
+                "table_corner.png",
+                "table_right.png",
+                "throbber.gif"]
       m.directory('public/images/schema_browser')
       images.each do |img|
         m.file "public/images/#{img}",  "public/images/schema_browser/#{img}"
       end
 
-      javascripts = ["ajax.js",
-                     "animator.js",
-                     "generic.js",
-                     "io.js",
-                     "main.js",
-                     "objects.js",
-                     "settings.js",
-                     "sql_types.js",
-                     "style.js"]
+      javascripts = ["config",
+                     "oz",
+                     "wwwsqldesigner"]
+
       m.directory('public/javascripts/schema_browser')
       javascripts.each do |js|
-        m.file "public/javascripts/#{js}", "public/javascripts/schema_browser/#{js}"
+        m.file "public/javascripts/#{js}.js", "public/javascripts/schema_browser/#{js}.js"
       end
 
-      stylesheets = ["bar.css",
-                     "foo",
-                     "style.css"]
+      stylesheets = ["doc",
+                     "ie6",
+                     "ie7",
+                     "print",
+                     "style"]
+
       m.directory('public/stylesheets/schema_browser')
       stylesheets.each do |css|
-        m.file "public/stylesheets/#{css}", "public/stylesheets/schema_browser/#{css}"
+        m.file "public/stylesheets/#{css}.css", "public/stylesheets/schema_browser/#{css}.css"
       end
 
+      locales = ["en"]
+      m.directory('public/xml/schema_browser/locales')
+      locales.each do |locale|
+        m.file "public/xml/locales/#{locale}.xml", "public/xml/schema_browser/locales/#{locale}.xml"
+      end
+
+      dbs = ["mysql", "sqlite"]
+      dbs.each do |db|
+        m.directory("public/xml/schema_browser/dbs/#{db}")
+        m.file "public/xml/dbs/#{db}/datatypes.xml", "public/xml/schema_browser/dbs/#{db}/datatypes.xml"
+        m.file "public/xml/dbs/#{db}/output.xsl", "public/xml/schema_browser/dbs/#{db}/output.xsl"
+      end
+      
       m.directory('app/controllers')
       m.file "app/controllers/schema_browser_controller.rb", "app/controllers/schema_browser_controller.rb"
       m.directory('app/views/schema_browser')
---
+++ generators/schema_browser/templates/public/javascripts/config.js
@@ -0,0 +1,13 @@
+var CONFIG = {
+	AVAILABLE_DBS:["mysql","sqlite"],
+	AVAILABLE_LOCALES:["en"],
+	
+	DEFAULT_DB:"mysql",
+	DEFAULT_LOCALE:"en",
+	
+	AVAILABLE_BACKENDS:["php-mysql","php-blank","php-file"],
+	DEFAULT_BACKEND:["php-mysql"],
+
+	RELATION_THICKNESS:2,
+	RELATION_SPACING:15
+}
