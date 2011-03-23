f46c546bd6c8083945e273e30369b7fcf02a427b
Tom ten Thij <git@tomtenthij.nl>
2011-03-23 15:37:41 +0000

    SchemaBrowser.reload will reload all the lib files in this gem (useful to effect changes in a dependent project when developing)

--- lib/schema_browser.rb
+++ lib/schema_browser.rb
@@ -1,2 +1,10 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+module SchemaBrowser
+  def self.reload
+    Dir.glob(File.join(File.dirname(__FILE__), "**", "*.rb")).each do |rb_file|
+      load rb_file
+    end
+  end
+end
