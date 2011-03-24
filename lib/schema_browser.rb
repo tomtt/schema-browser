0e939cba3726a9f13f3aa72a50f63812ad6bf28a
Tom ten Thij <git@tomtenthij.nl>
2011-03-24 14:43:14 +0000

    SchemaBrowser::Rails::Reflection::models will return all the models in the current rails environment

--- lib/schema_browser.rb
+++ lib/schema_browser.rb
@@ -1,10 +1,12 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require "schema_browser/rails/reflection"
+
 module SchemaBrowser
   def self.reload
     Dir.glob(File.join(File.dirname(__FILE__), "**", "*.rb")).each do |rb_file|
-      load rb_file
+      require rb_file
     end
   end
 end
