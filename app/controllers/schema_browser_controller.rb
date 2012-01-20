81aaf45e74d3008899911cf3182cb1a5c114f02b
Tom ten Thij <git@tomtenthij.nl>
2012-01-20 17:37:46 +0000

    /schema_browser/schema can return xml and json representations of the schema data

--- app/controllers/schema_browser_controller.rb
+++ app/controllers/schema_browser_controller.rb
@@ -1,3 +1,11 @@
 class SchemaBrowserController < ApplicationController
   layout "schema_browser"
+
+  def schema
+    schema_data = SchemaBrowser::RailsInspector::Schema.generate_from_rails
+    respond_to do |format|
+      format.xml { render :xml => SchemaBrowser::SchemaFormatter.new(schema_data).to_www_sql_designer_xml }
+      format.json { render :json => schema_data.to_json }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   match 'schema_browser' => 'schema_browser#index'
+  match 'schema_browser/schema' => 'schema_browser#schema'
 end
